e097f89c5a092ba5a62bcb2b0d61ff90dc3359c3
KooshinB <44259077+KooshinB@users.noreply.github.com>
2018-10-25 15:30:32 +0100

    Add files via upload

---
+++ Dragging_Image/Dragging_Image/DraggedImageView.swift
@@ -0,0 +1,31 @@
+//
+//  DraggedImageView.swift
+//  Dragging_Image
+//
+//  Created by ka15abc on 25/10/2018.
+//  Copyright © 2018 CSStestuser. All rights reserved.
+//
+
+import UIKit
+
+class DraggedImageView: UIImageView {
+    
+    var startLocation: CGPoint?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        startLocation = touches.first?.location(in: self)
+        
+        
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let currentLocation = touches.first?.location(in: self)
+        let dx = currentLocation!.x - startLocation!.x
+        let dy = currentLocation!.y - startLocation!.y
+        
+        self.center = CGPoint(x: self.center.x+dx,y: self.center.y+dy)
+    }
+    
+    
+}
---
+++ Dragging_Image/Dragging_Image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dragging_Image
+//
+//  Created by ka15abc on 25/10/2018.
+//  Copyright © 2018 CSStestuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
